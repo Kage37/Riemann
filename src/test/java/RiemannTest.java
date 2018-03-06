16a9c68c6843a38f97c5e24b402b5f9eb0def4a4
Kage37 <Kage37@users.noreply.github.com>
2018-03-05 20:19:07 -0500

    Tests Added and Object EquationData Implemented

--- src/test/java/RiemannTest.java
+++ src/test/java/RiemannTest.java
@@ -33,13 +33,75 @@ public class RiemannTest {
 
         //results
 
-        // wont' compile until you create getSum method double sum = solver.getSum(simplestProblem);
+        /*wont' compile until you create getSum method */
+        double sum = solver.getSum(simplestProblem);
 
-        //Assert.assertEquals(50.0d, sum, 0.0001d);
+        Assert.assertEquals(50.0d, sum, 0.0001d);
 
-        assertTrue(false);
-
+        //assertTrue(false);
     }
+    
+   @Test
+    public void testRiemannWithLeftEquation(){
+        //inputs
 
+        EquationData problem = new EquationData(
+                "1-x^4+3x^2",
+                Riemann.LEFT,
+                -5.0d,
+                10.0d,
+                20
+        );
 
+        Riemann solver = new Riemann();
+
+        //results
+
+        double sum = solver.getSum(problem);
+
+        Assert.assertEquals( -16260.310546875d, sum, 0.0001d);
+   }
+       
+   @Test
+    public void testRiemannWithRightEquation(){
+        //inputs
+
+        EquationData problem = new EquationData(
+                "1-2x^4+5x^2",
+                Riemann.RIGHT,
+                -52.0d,
+                17.0d,
+                23
+        );
+
+        Riemann solver = new Riemann();
+
+        //results
+
+        double sum = solver.getSum(problem);
+
+        Assert.assertEquals( -131613015d, sum, 0.0001d);
+   }
+   
+       
+   @Test
+    public void testRiemannWithHardEquation(){
+        //inputs
+
+        EquationData problem = new EquationData(
+                "2/3x^9-1/7x^8+23/4x^7-67x^6-1/3x^5+233x^4+7x^3-x^2-99/7x-100",
+                Riemann.MIDPOINT,
+                0d,
+                1d,
+                100
+        );
+
+        Riemann solver = new Riemann();
+
+        //results
+
+        double sum = solver.getSum(problem);
+
+        Assert.assertEquals( -67.91467089d, sum, 0.0000001d);
+   }
 }
